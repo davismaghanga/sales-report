23acea0228ad72643847fb38d8a70fcfb01536d9
devosuka <dkirughamio@gmail.com>
2018-11-07 14:49:33 +0300

    more pagination - subcounties and uploaded institutions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -274,7 +274,7 @@ class AdminController extends Controller
 
     public function allsubcounties()
     {
-        $subcounties = SubCounty::all();
+        $subcounties = SubCounty::paginate(50);
         return view('admin.pages.subcounties', compact('subcounties'));
 
     }
